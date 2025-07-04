1360b42f9c02f7fb806f0f6d3e63a927035573c3
Elmurod <98465953+Elmur0d@users.noreply.github.com>
2025-07-04 06:45:25 +0500

    melkie izmeneniya

--- server/config/db.js
+++ server/config/db.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 module.exports = new Sequelize(
-  process.env.DB_NAME,     // Название БД
-  process.env.DB_USER,     // Пользователь
-  process.env.DB_PASSWORD, // Пароль
+  process.env.DB_NAME,     
+  process.env.DB_USER,     
+  process.env.DB_PASSWORD, 
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'postgres',
-    logging: false // Отключаем логи SQL-запросов
+    logging: false 
   }
 );
