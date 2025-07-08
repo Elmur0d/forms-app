81e57b6a66f29a500ff78846af6d2513f733f7a6
Elmurod <98465953+Elmur0d@users.noreply.github.com>
2025-07-08 06:46:30 +0500

    testing again

--- server/routes/questionRoutes.js
+++ server/routes/questionRoutes.js
@@ -2,10 +2,13 @@ import express from 'express';
 import {
   updateQuestion,
   deleteQuestion,
+  reorderQuestions,
 } from '../controllers/questionController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
+
+router.route('/reorder').put(protect, reorderQuestions);
 
 router
   .route('/:id')
