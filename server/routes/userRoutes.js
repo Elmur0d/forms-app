b38052ff2b0d03b1744aa71c3874846d654f02d0
Elmurod <98465953+Elmur0d@users.noreply.github.com>
2025-07-19 23:23:40 +0500

    fixing blocking/unblocking func

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,13 +1,20 @@
 import express from 'express';
-import { getUsers, toggleAdmin, deleteUser, searchUsers, toggleBlock  } from '../controllers/userController.js';
+import {
+  getUsers,
+  toggleAdmin,
+  deleteUser,
+  searchUsers,
+  toggleBlock 
+} from '../controllers/userController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
+router.route('/search').get(protect, searchUsers);
+
 router.route('/').get(protect, admin, getUsers);
 router.route('/:id/toggle-admin').put(protect, admin, toggleAdmin);
-router.route('/:id/toggle-block').put(toggleBlock);
+router.route('/:id/toggle-block').put(protect, admin, toggleBlock);
 router.route('/:id').delete(protect, admin, deleteUser);
-router.route('/search').get(protect, searchUsers);
 
 export default router;
